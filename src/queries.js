32e4cadf2c6dfbd9179629bca17a92b8bd0b150d
Muhammad Nor Arif Bin Zakaria (Digital) <aripian.android@gmail.com>
2019-04-24 10:32:16 +0800

    fixed bug

--- src/queries.js
+++ src/queries.js
@@ -1,4 +1,4 @@
-const insertTrans = 'INSERT INTO my_trans VALUES (DEFAULT, $1, $2, $3, $4, $5, $6, $7, DEFAULT) RETURNING *;';
+const insertTrans = 'INSERT INTO my_trans VALUES (DEFAULT, $1, $2, $3, $4, $5, $6, $7, $8, DEFAULT) RETURNING *;';
 const getOrder = 'SELECT * FROM my_order WHERE id = $1;';
 const updateStatus = 'UPDATE my_order SET status = $1 WHERE token = $2 AND id = $3;';
 
